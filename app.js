a17887615d5c3a265adc4279a31d3189894cb249
jiananWang <a0918666589@gmail.com>
2018-05-06 14:23:49 +0800

    add(categories) add categories page and routes on admin side

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var dashboardRouter = require('./routes/dashboard');
@@ -14,6 +16,15 @@ app.engine('ejs', require('express-ejs-extend'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'cyborg',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 100 * 1000
+  }
+}));
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const firebaseAdmin = require('../services/firebase-admin');
+
+const router = express.Router();
+const categoriesRef = firebaseAdmin.ref('/categories/');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -10,12 +13,48 @@ router.get('/article', function (req, res, next) {
   res.render('dashboard/article');
 });
 
-router.get('/signup', function (req, res, next) {
-  res.render('dashboard/signup');
+router.get('/categories', function (req, res, next) {
+  const messages = req.flash('notification');
+  categoriesRef.once('value')
+    .then((snapshot) => {
+      const categories = snapshot.val();
+      res.render('dashboard/categories', {
+        categories,
+        messages,
+        hasNotification: messages.length > 0
+      });
+    })
 });
 
-router.get('/categories', function (req, res, next) {
-  res.render('dashboard/categories');
+router.post('/categories/create', (req, res, next) => {
+  const data = req.body;
+  const categoryRef = categoriesRef.push();
+  const key = categoryRef.key;
+  data.id = key;
+  categoriesRef.orderByChild('path').equalTo(data.path).once('value')
+    .then((snapshot) => {
+      if (snapshot.val()) {
+        req.flash('notification', '已有相同路徑')
+        res.redirect('/dashboard/categories');
+      } else {
+        categoryRef.set(data)
+          .then(() => {
+            res.redirect('/dashboard/categories');
+          })
+      }
+    });
+})
+
+router.post('/categories/delete/:id', (req, res, next) => {
+  const id = req.param('id');
+  console.log(id);
+  categoriesRef.child(id).remove();
+  req.flash('notification', '刪除標籤分類');
+  res.redirect('/dashboard/categories');
+})
+
+router.get('/signup', function (req, res, next) {
+  res.render('dashboard/signup');
 });
 
 module.exports = router;
